3f7a575edb26599ca257884c3fb33c31a8c6a322
Anatolii Kravchenko <tol890@ukr.net>
2022-06-04 21:09:30 +0300

    Implement get city names function

---
+++ main.rb
@@ -0,0 +1,35 @@
+dial_book = {
+    "newyork" => "212",
+    "newbrunswich" => "732",
+    "edison" => "908",
+    "plainsboro" => "609",
+    "sanfrancisco" => "301",
+    "miami" => "305",
+    "paloalto" => "650",
+    "evanston" => "847",
+    "orlando" => "407",
+    "lancaster" => "717"
+}
+
+# Get city names from the hash
+def get_city_names(somehash)
+   somehash.keys
+end
+
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+
+end
+
+# Execution flow
+loop do
+    puts "Do you want to lookup an area code based on a city name? (Y/N)"
+    answer = gets.chomp.downcase
+    break if answer != "y"
+    puts "Which city do you wand the area code for?"
+    puts get_city_names(dial_book)
+    puts "Enter your selection"
+    requested_city = gets.chomp.downcase
+    get_area_code(dial_book, requested_city)
+    
+end
